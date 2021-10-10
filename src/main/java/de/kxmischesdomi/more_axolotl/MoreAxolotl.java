4a97de8854afbed92f690b49b33995c1474d42b7
KxmischesDomi <kxmischesdomi@gmail.com>
2021-10-10 22:31:18 +0200

    Add catalog trade

--- src/main/java/de/kxmischesdomi/more_axolotl/MoreAxolotl.java
+++ src/main/java/de/kxmischesdomi/more_axolotl/MoreAxolotl.java
@@ -2,6 +2,11 @@ package de.kxmischesdomi.more_axolotl;
 
 import de.kxmischesdomi.more_axolotl.common.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.VillagerProfession;
 
 public class MoreAxolotl implements ModInitializer {
 
@@ -11,8 +16,9 @@ public class MoreAxolotl implements ModInitializer {
 	public void onInitialize() {
 		ModItems.init();
 
-
-
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FISHERMAN, 1, factories -> {
+			factories.add((entity, random) -> new TradeOffer(new ItemStack(Items.BOOK), new ItemStack(Items.EMERALD, 3), new ItemStack(ModItems.AXOLOTL_CATALOG), 0, 1, 3, 0));
+		});
 
 	}
 }
