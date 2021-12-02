9af4346dca2031b284fe98de28b4af1bc9ab757a
KxmischesDomi <kxmischesdomi@gmail.com>
2021-12-02 23:49:43 +0100

    Fix dedicated server crash

--- src/main/java/de/kxmischesdomi/more_axolotls/MoreAxolotlsClient.java
+++ src/main/java/de/kxmischesdomi/more_axolotls/MoreAxolotlsClient.java
@@ -1,12 +1,16 @@
 package de.kxmischesdomi.more_axolotls;
 
+import de.kxmischesdomi.more_axolotls.client.screen.AxolotlCatalogScreen;
 import de.kxmischesdomi.more_axolotls.common.AxolotlVariantManager;
+import de.kxmischesdomi.more_axolotls.common.item.AxolotlCatalogItem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.animal.axolotl.Axolotl;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +25,11 @@ public class MoreAxolotlsClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+
+		AxolotlCatalogItem.openCatalogAction = player -> {
+			AxolotlCatalogScreen screen = new AxolotlCatalogScreen(player.level);
+			Minecraft.getInstance().setScreen(screen);
+		};
 
 		for (Axolotl.Variant variant : Axolotl.Variant.values()) {
 			if (variant.getId() == 0) continue;
