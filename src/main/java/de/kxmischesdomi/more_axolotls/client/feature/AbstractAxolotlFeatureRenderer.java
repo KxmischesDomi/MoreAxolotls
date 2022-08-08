d42fc78d3647dda8256f3d77153b5685951105ec
KxmischesDomi <kxmischesdomi@gmail.com>
2022-08-08 16:23:58 +0200

    Some changes .-.

--- src/main/java/de/kxmischesdomi/more_axolotls/client/feature/AbstractAxolotlFeatureRenderer.java
+++ src/main/java/de/kxmischesdomi/more_axolotls/client/feature/AbstractAxolotlFeatureRenderer.java
@@ -65,7 +65,7 @@ public abstract class AbstractAxolotlFeatureRenderer extends RenderLayer<Axolotl
 			String name = entity.getName().getContents();
 
 			for (Map.Entry<String, Axolotl.Variant> entry : AxolotlVariantManager.CUSTOM_NAME_VARIANTS.get().entrySet()) {
-				if (name.equals(entry.getKey())) {
+				if (name.equalsIgnoreCase(entry.getKey())) {
 					return entry.getValue().getName();
 				}
 			}
--- src/main/java/de/kxmischesdomi/more_axolotls/common/item/AxolotlCatalogItem.java
+++ src/main/java/de/kxmischesdomi/more_axolotls/common/item/AxolotlCatalogItem.java
@@ -1,12 +1,19 @@
 package de.kxmischesdomi.more_axolotls.common.item;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 /**
@@ -29,6 +36,14 @@ public class AxolotlCatalogItem extends Item {
 		}
 
 		return InteractionResultHolder.pass(user.getItemInHand(hand));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+		MutableComponent component = Component.translatable("item.more-axolotls.axolotl_catalog.tooltip");
+		component.setStyle(Style.EMPTY.withItalic(true).withColor(ChatFormatting.GRAY));
+		list.add(component);
+		super.appendHoverText(itemStack, level, list, tooltipFlag);
 	}
 
 }
